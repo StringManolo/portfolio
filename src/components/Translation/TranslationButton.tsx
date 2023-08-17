2f5972b50673b4cf4cef5235502697f9e0b19156
StringManolo <manuelvarelacaldas@gmail.com>
2023-08-17 13:28:47 +0000

    Change translate buttons on PC

--- src/components/Translation/TranslationButton.tsx
+++ src/components/Translation/TranslationButton.tsx
@@ -1,6 +1,8 @@
 import { useLanguage } from './LanguageContext';
-
+import { useEffect, useRef } from 'react';
 import styles from './TranslationButton.module.sass';
+
+
 
 const TranslationButton = () => {
   const { language, changeLanguage } = useLanguage();
@@ -12,8 +14,35 @@ const TranslationButton = () => {
   const showOpositeLanguage = (lang: string) => {
     return (lang === 'en' ? 'es' : 'en').toUpperCase();
   }
+
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const content = contentRef.current;
+
+    function resizeContent() {
+      const windowHeight = document.body.scrollHeight;
+      //const contentHeight = content.clientHeight;
+
+      //if (contentHeight < windowHeight) {
+      // @ts-ignore
+      content.style.height = windowHeight + 'px';
+      //}
+    }
+
+    window.addEventListener('resize', resizeContent);
+    resizeContent();
+
+    return () => {
+      window.removeEventListener('resize', resizeContent);
+    };
+  }, []);
+
+
   return (
-    <button className={styles['translation-button']} onClick={toggleLanguage}>{showOpositeLanguage(language)}</button>
+    <div className={styles['translation-button-container']} ref={contentRef}>
+      <button className={styles['translation-button']} onClick={toggleLanguage}>{showOpositeLanguage(language)}</button>
+    </div>
   )
 }
 
